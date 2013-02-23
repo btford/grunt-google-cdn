0d52bac17f65f81929292fd3343a09ac1e50a9a3
Brian Ford <btford@umich.edu>
2013-02-22 21:46:56 -0500

    add support for versions

--- tasks/cdnify.js
+++ tasks/cdnify.js
@@ -1,5 +1,6 @@
-// i am not good at naming things
 'use strict';
+
+var semver = require('semver');
 
 module.exports = function (grunt) {
 
@@ -8,6 +9,8 @@ module.exports = function (grunt) {
     // collect files
     var files = grunt.file.expand({ filter: 'isFile' }, this.data.html);
     var dest = options.dest;
+
+    var compJson = grunt.file.readJSON('component.json');
 
     grunt.log
       .writeln('Going through ' + grunt.log.wordlist(files) + ' to update script refs');
@@ -26,24 +29,45 @@ module.exports = function (grunt) {
 
     var angularFiles = [
       'angular',
-      'angular-resource',
       'angular-cookies',
+      'angular-loader',
+      'angular-resource',
       'angular-sanitize'
     ];
 
+    // Build up a map of versions supported by Google's CDN
+    // scraped from: https://developers.google.com/speed/libraries/devguide
+    var versions = {
+      'jQuery': ['1.9.1', '1.9.0', '1.8.3', '1.8.2', '1.8.1', '1.8.0', '1.7.2', '1.7.1', '1.7.0', '1.6.4', '1.6.3', '1.6.2', '1.6.1', '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.2', '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.6', '1.2.3']
+    };
+    var angularVersions = ['1.0.4', '1.0.3', '1.0.2', '1.0.1'];
+    angularFiles.forEach(function (file) {
+      versions[file] = angularVersions;
+    });
+
     // TODO: versions
     angularFiles.forEach(function (item) {
-      replacements['components/' + item + '/' + item + '.js' ] =
-        '//ajax.googleapis.com/ajax/libs/angularjs/1.0.4/' + item + '.min.js';
+      replacements[item] = {
+        from: 'components/' + item + '/' + item + '.js',
+        to: function (version) {
+          return '//ajax.googleapis.com/ajax/libs/angularjs/' + version + '/' + item + '.min.js';
+        }
+      };
     });
 
     files.forEach(function (file) {
       var content = file.body;
-      grunt.util._.each(replacements, function (cdnRef, bowerRef) {
-        content = content.replace(bowerRef, cdnRef);
+      grunt.util._.each(replacements, function (rep, name) {
+        var versionStr = compJson.dependencies[name] || compJson.devDependencies[name];
+        if (!versionStr) {
+          return;
+        }
+        var version = semver.maxSatisfying(versions[name], versionStr);
+        content = content.replace(rep.from, rep.to(version));
       });
 
       grunt.file.write(file.path, content);
     });
+
   });
 };
