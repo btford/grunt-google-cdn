c92e28727a3e9111d5883961baacc1d05b3ab343
Brian Ford <btford@umich.edu>
2013-02-24 15:31:46 -0500

    move version data out of main task

--- tasks/cdnify.js
+++ tasks/cdnify.js
@@ -3,6 +3,13 @@
 var semver = require('semver');
 var hoist = require('../util/hoist.js');
 
+
+// map refs from bower -> cdn
+var replacements = require('../lib/data.js').replacements;
+
+// map of lib name -> Google CDN supported versions
+var versions = require('../lib/data.js').versions;
+
 module.exports = function (grunt) {
 
   grunt.registerMultiTask('cdnify', 'replace scripts with refs to the Google CDN', function () {
@@ -22,44 +29,6 @@ module.exports = function (grunt) {
         body: grunt.file.read(filepath)
       };
     });
-
-    // map refs from bower -> cdn
-    var replacements = {};
-
-    // TODO: jQuery, Dojo, etc.
-
-    var angularFiles = [
-      'angular',
-      'angular-cookies',
-      'angular-loader',
-      'angular-resource',
-      'angular-sanitize'
-    ];
-
-    // Build up a map of versions supported by Google's CDN
-    // scraped from: https://developers.google.com/speed/libraries/devguide
-    var versions = {
-      'jQuery': ['1.9.1', '1.9.0', '1.8.3', '1.8.2', '1.8.1', '1.8.0', '1.7.2', '1.7.1', '1.7.0', '1.6.4', '1.6.3', '1.6.2', '1.6.1', '1.6.0', '1.5.2', '1.5.1', '1.5.0', '1.4.4', '1.4.3', '1.4.2', '1.4.1', '1.4.0', '1.3.2', '1.3.1', '1.3.0', '1.2.6', '1.2.3']
-    };
-    var angularVersions = ['1.0.4', '1.0.3', '1.0.2', '1.0.1'];
-    angularFiles.forEach(function (file) {
-      versions[file] = angularVersions;
-    });
-
-    angularFiles.forEach(function (item) {
-      replacements[item] = {
-        from: 'components/' + item + '/' + item + '.js',
-        to: function (version) {
-          return '//ajax.googleapis.com/ajax/libs/angularjs/' + version + '/' + item + '.min.js';
-        }
-      };
-    });
-    replacements['jquery'] = {
-      from: 'components/jquery/jquery.js',
-      to: function (version) {
-        return '//ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.min.js';
-      }
-    };
 
     files.forEach(function (file) {
       var content = file.body;
