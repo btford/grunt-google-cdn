42737fbc5f1d2d410d1689d33444e2e1ffb1892f
Pascal Hartig <phartig@rdrei.net>
2013-06-03 17:13:33 +0200

    Added regression test for #25

--- test/bower_test.js
+++ test/bower_test.js
@@ -1,18 +1,25 @@
 'use strict';
 
 var proxyquire = require('proxyquire');
+var path = require('path');
 
 exports.bower = {
 
   setUp: function (cb) {
     this.bowerConfig = {};
 
+    this.pathMock = {
+      join: path.join,
+      sep: '/'
+    };
+
     this.bowerMock = {
       config: this.bowerConfig
     };
 
     this.bowerUtil = proxyquire('../util/bower', {
-      bower: this.bowerMock
+      bower: this.bowerMock,
+      path: this.pathMock
     });
 
     cb();
@@ -22,6 +29,16 @@ exports.bower = {
     this.bowerConfig.directory = 'app/bower_components';
 
     var result = this.bowerUtil.joinComponent('jquery/jquery-2.0.js');
+    test.equal(result, 'bower_components/jquery/jquery-2.0.js');
+
+    test.done();
+  },
+
+  joinComponentW32Regression: function (test) {
+    this.pathMock = '\\';
+    this.bowerConfig.directory = 'app/bower_components';
+
+    var result = this.bowerUtil.joinComponent('jquery\\jquery-2.0.js');
     test.equal(result, 'bower_components/jquery/jquery-2.0.js');
 
     test.done();
